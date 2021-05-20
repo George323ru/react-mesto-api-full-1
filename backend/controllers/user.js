c5142407969d92057ddd2810190ae375135f3f9e
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-05-20 21:41:25 +0300

    feat: fix lint errors

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -74,7 +74,8 @@ module.exports.login = (req, res, next) => {
         const token = jwt.sign(
           { _id: user._id },
           NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
-          { expiresIn: '7d' });
+          { expiresIn: '7d' },
+        );
         res.send({ token });
       }))
     .catch((error) => {
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key'
+      NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
     );
   } catch (error) {
     throw new MineError('Необходима авторизация', 401);
